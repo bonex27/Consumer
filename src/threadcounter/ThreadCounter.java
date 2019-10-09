759193e51eade505ac0be151d792bbbdde4728ee
Bonex <informatica@lai2>
2019-10-09 11:49:18 +0200

    Starting thread with RC problems

--- src/threadcounter/ThreadCounter.java
+++ src/threadcounter/ThreadCounter.java
@@ -15,23 +15,14 @@ public class ThreadCounter {
      * @param args the command line arguments
      */
     
-<<<<<<< HEAD
     public static int count=0;
-=======
-     static int count=0;
->>>>>>> dffe6a10658b0393f178eb513ac34b667d1440a6
     public static void main(String[] args) 
     {
-        for(int i=0;i<1000;i++)
-        {
         Thread Counter=new Thread(new Counter());
-        //Thread Printer=new Thread(new Printer());
-        }
-
+        Thread Printer=new Thread(new Printer());
+        Counter.start();
+        Printer.start();
         
-        
-        //Counter.start();
-        //Printer.start();
     }
     
 }
