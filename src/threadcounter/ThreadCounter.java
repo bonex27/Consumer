5edc6b283851094ae918aab5e69278be4a3fcac8
lai2 <informatica@lai2>
2019-10-09 11:31:12 +0200

    new class main

--- src/threadcounter/ThreadCounter.java
+++ src/threadcounter/ThreadCounter.java
@@ -15,11 +15,14 @@ public class ThreadCounter {
      * @param args the command line arguments
      */
     
-    public int count=0;
+    public static int count=0;
     public static void main(String[] args) 
     {
+        for(int i=0;i<1000;i++)
+        {
         Thread Counter=new Thread(new Counter());
         Thread Printer=new Thread(new Printer());
+        }
         
         
         Counter.start();
